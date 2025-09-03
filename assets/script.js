c3b6bf844fadf8f6a63a0ac12d5c6bed29e0212c
GUILLE sPAGNOLO <guillermospagnolo10@gmail.com>
2025-09-03 11:56:09 -0300

    correcciones de logos en todos los htmls, orden de css y js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Contenido original del main
+    // main original
     const originalMainContent = `
         <div class="side-img">
             <img src="/components/Lado.jpg" alt="Imagen lateral">
@@ -8,10 +8,10 @@ document.addEventListener('DOMContentLoaded', function() {
         <div class="menu-buttons">
             <button class="btn horarios-btn" style="background-image: url('/components/horarios.webp');">HORARIOS</button>
             <button class="btn" style="background-image: url('/components/actividades.webp');">ACTIVIDADES</button>
-            <button class="btn" style="background-image: url('/components/planes.webp');">PLANES</button>
-            <button class="btn" style="background-image: url('/components/contacto.webp');">CONTACTO</button>
-            <button class="btn" style="background-image: url('/components/ubicacion.webp');">UBICACION</button>
-            <button class="btn" style="background-image: url('/components/extras.webp');">EXTRAS</button>
+            <a href="/views/planes.html "><button class="btn" style="background-image: url('/components/planes.webp');">PLANES</button></a>
+            <a href="/views/Contacto.html"><button class="btn" style="background-image: url('/components/contacto.webp');">CONTACTO</button></a>
+            <a href="https://maps.app.goo.gl/Se9y4yyksf6xyDjt9"><button class="btn" style="background-image: url('/components/ubicacion.webp');">UBICACION</button></a>
+            <a href="/views/sobreNosotros.html"><button class="btn" style="background-image: url('/components/extras.webp');">SOBRE NOSOTROS</button></a>
         </div>
 
         <div class="side-img">
